93431e836fce6f44d6a47377dec1932f3f3cdfea
chanan-duy <chanan.duy@proton.me>
2025-10-28 20:38:25 +0500

    `Program` simple console login and password read

--- CredentialsValidator/Program.cs
+++ CredentialsValidator/Program.cs
@@ -2,12 +2,49 @@
 
 namespace CredentialsValidator;
 
+public static class MainLogic
+{
+    public static void Run()
+    {
+        PrintNewLine();
+        Console.WriteLine("Credentials validator");
+        PrintNewLine();
+
+        RunLogin();
+
+        PrintNewLine();
+
+        RunPassword();
+    }
+
+    private static void RunLogin()
+    {
+        var loginValidator = new LoginValidator();
+
+        Console.WriteLine("Enter login:");
+        var login = Console.ReadLine()!;
+        Console.WriteLine($"is login valid?: {loginValidator.IsValid(login)}");
+    }
+
+    private static void RunPassword()
+    {
+        var passwordValidator = new PasswordValidator();
+
+        Console.WriteLine("Enter password:");
+        var password = Console.ReadLine()!;
+        Console.WriteLine($"is password valid?: {passwordValidator.IsValid(password)}");
+    }
+
+    private static void PrintNewLine()
+    {
+        Console.Write(Environment.NewLine);
+    }
+}
+
 internal static class Program
 {
     private static void Main(string[] _)
     {
-        var val = new LoginValidator();
-
-        Console.WriteLine(val.IsValid("a"));
+        MainLogic.Run();
     }
 }
